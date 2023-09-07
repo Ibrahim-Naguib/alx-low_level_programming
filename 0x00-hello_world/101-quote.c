70dff15f09746fa63b61a8748b2baf642c5cc34a
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-07 10:34:43 -0700

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -7,6 +8,6 @@
 int main(void)
 {
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-	write(2, quote, sizeof(quote));
+	write(1, quote, sizeof(quote));
 	return (1);
 }
