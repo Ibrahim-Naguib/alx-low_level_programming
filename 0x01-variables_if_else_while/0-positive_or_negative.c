e5422cdad6da3bb6a568d79c908fefc97628c7a0
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-08 09:13:07 +0000

    new commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
-  main - Entry point
-
-Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
