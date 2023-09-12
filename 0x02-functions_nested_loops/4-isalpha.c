9323c309f31220bb9de52238b85f12462bc58658
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-12 11:43:26 +0000

    new commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isalpha - Checks if c is lowercase
+ *
+ * @c: The character to check.
+ *
+ * Return: 1 if lower, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
