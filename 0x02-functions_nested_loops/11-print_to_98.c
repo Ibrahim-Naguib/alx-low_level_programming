ac2677b2185c0aa01b0a15c2fd3e0bccf73524d8
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-12 17:41:52 +0000

    new commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,30 @@ void print_to_98(int n)
 {
 	int i, j;
 
-	for (i = n; i <= 98; i++)
+	if (n == 98)
 	{
-		printf("%d, \n", i);
-		if (i != 98)
-		{
-			printf(", ");
-		}
+		printf("98\n");
 	}
-	for (j = n; j >= 98; j--)
+	else if (n < 98)
 	{
-		printf("%d, \n", j);
-		if (j != 98)
+		for (i = n; i <= 98; i++)
 		{
-			printf(", ");
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
 		}
+		else
+		{
+			for (j = n; j >= 98; j--)
+			{
+				printf("%d", j);
+				if (j != 98)
+				{
+					printf(", ");
+				}
+			}
+		}
+		printf("\n");
 	}
-	printf("\n");
-}
