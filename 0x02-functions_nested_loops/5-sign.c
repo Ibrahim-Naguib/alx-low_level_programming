5266c310d9e4a22559a317d17ef8e5aa2a828a65
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-12 12:22:23 +0000

    new commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,29 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n + '0');
-		_putchar('\n');
+		_putchar(43);
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n + '0');
-		_putchar('\n');
+		_putchar(48);
 		return (0);
 	}
 	else
 	{
-		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n + '0');
-		_putchar('\n');
+		_putchar(45);
 		return (-1);
 	}
 }
