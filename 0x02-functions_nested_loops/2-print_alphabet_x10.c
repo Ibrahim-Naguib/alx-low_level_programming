6e3b7d954178e347c2036d65b9360a6920c04385
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-12 11:24:00 +0000

    new commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet 10 times
+ * print_alphabet_x10 - Prints the alphabet 10 times
  *
  * Return: Void.
  */
@@ -17,5 +17,7 @@ void print_alphabet_x10(void)
 			alpha++;
 		}
 	_putchar('\n');
+	alpha = 'a';
+	i++;
 	}
 }
