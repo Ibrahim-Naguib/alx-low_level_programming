c178e6cb0ea5be9ac326ace149d26de3ee3262ca
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-12 12:50:53 +0000

    new commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last number
+ *
+ * @n: The number to check.
+ *
+ * Return: last number.
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
