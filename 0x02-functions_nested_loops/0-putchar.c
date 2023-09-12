1e8d42d5ba80cca27e9d1beb752b0fa68a094d2b
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-12 10:49:18 +0000

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	_putchar(str);
+	return (0);
+}
+void _putchar(char str[])
+{
+	write(STDOUT_FILENO, str, sizeof((str) - 1));
+}
