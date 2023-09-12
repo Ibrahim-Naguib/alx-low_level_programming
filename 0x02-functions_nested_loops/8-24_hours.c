0bbee18602607839ad7c390de5a33ab3eac1aed6
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-12 15:01:21 +0000

    new commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,12 +12,12 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-				_putchar('0' + (hour / 10));
-				_putchar('0' + (hour % 10));
-				_putchar(':');
-				_putchar('0' + (minute / 10));
-				_putchar('0' + (minute % 10));
-				_putchar('\n');
+			_putchar('0' + (hour / 10));
+			_putchar('0' + (hour % 10));
+			_putchar(':');
+			_putchar('0' + (minute / 10));
+			_putchar('0' + (minute % 10));
+			_putchar('\n');
 		}
 	}
 }
