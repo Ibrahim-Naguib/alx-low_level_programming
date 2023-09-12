8741c54c01e565c856dac6bd3003fef8235b9603
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-12 16:50:26 +0000

    new commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,24 +13,27 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			multi = i * j;
+			if (j > 0)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 			if (multi < 10)
 			{
-				_putchar('0' + multi);
+				putchar(' ');
+			}
+			if (multi < 10)
+			{
+				putchar('0' + multi);
 			}
 			else
 			{
-				_putchar('0' + multi / 10);
-				_putchar('0' + multi % 10);
+				putchar('0' + multi / 10);
+				putchar('0' + multi % 10);
 
 			}
 
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
