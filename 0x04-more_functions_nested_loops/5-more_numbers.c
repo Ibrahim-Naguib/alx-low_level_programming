801424e1e886096c9d1106ad36f64868c6f7197a
Ibrahim Naguib <80571858+Ibrahim-Naguib@users.noreply.github.com>
2023-09-14 15:07:28 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,17 @@
  * It does not take any parameters and does not return a value.
  */
 
-void more_numbers(void)
-{
-	int i;
+void more_numbers(void) {
+    // Prints the numbers from 0 to 14.
+    int i, j;
 
-	for (i = 0; i <= 14; i++)
-	{
-		if (i >= 10)
-		{
-			_putchar((i / 10) + '0');
-		}
-		_putchar((i % 10) + '0');
-	}
-	_putchar('\n');
-}
+    for (i = 0; i <= 14; i++)
+        for (j = 0; j <= 14; j++) {
+            if (i >= 10) {
+                _putchar((i / 10) + '0');
+            }
+            _putchar((i % 10) + '0');
+        }
+        _putchar('\n');
+    }
 
