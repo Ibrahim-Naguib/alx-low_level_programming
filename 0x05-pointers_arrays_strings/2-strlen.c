46c9c3c85fa47b8d1b0eb40c808915eed6f95551
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-18 18:39:26 +0000

    new commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Computes the length of a string.
+ *
+ * @s: The string to compute the length of.
+ *
+ * Return: The length of the string.
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
