88ded45dfcdcf23a8195285fe58a9318ea5bcb3a
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-20 23:06:37 +0000

    new commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,23 +15,11 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] < s2[i])
+		if (s1[i] != s2[i])
 		{
-			return (-15);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (15);
+			return  (s1[i] - s2[i]);
 		}
 		i++;
-	}
-	if (s1[i] == '\0' && s2[i] != '\0')
-	{
-		return (-15);
-	}
-	else if (s1[i] != '\0' && s2[i] == '\0')
-	{
-		return (15);
 	}
 	return (0);
 }
