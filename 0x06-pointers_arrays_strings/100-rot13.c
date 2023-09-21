46a0ff4027159c930c52f7b849222f42c59c6a9d
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-21 19:37:25 +0000

    new commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,25 +10,19 @@
 char *rot13(char *str)
 {
 	int i = 0, j = 0;
-	char firstHalf[] = "ABCDEFGHIJKLMabcdefghijklm";
-	char secondHalf[] = "NOPQRSTUVWXYZnopqrstuvwxyz";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char alpha2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (str[i] == firstHalf[j])
+			if (str[i] == alpha[j])
 			{
-				str[i] = secondHalf[j];
-				break;
-			}
-			else if (str[i] == secondHalf[j])
-			{
-				str[i] = firstHalf[j];
+				str[i] = alpha2[j];
 				break;
 			}
 		}
 	}
-
 	return (str);
 }
