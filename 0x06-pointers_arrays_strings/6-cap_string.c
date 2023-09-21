096180997882915feffd74b22f38c83ef3f0618a
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-21 11:42:03 +0000

    new commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes words.
+ *
+ * @str: The string to be modified.
+ *
+ * Return: A pointer to the modified string.
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+	int digit;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] < 'A')
+		{
+			if (str[i] >= '0' && str[i] <= '9')
+			{
+				digit = 1;
+			}
+			else if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				if (!digit)
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
+				digit = 0;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
