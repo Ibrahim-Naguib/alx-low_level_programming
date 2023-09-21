0d69857303416f83f036aea799cd864d85112a3c
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-21 17:06:11 +0000

    new commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@
 
 char *leet(char *str)
 {
-	int i = 0, j = 0, k;
+	int i = 0, j = 0;
 	char leet[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int nums[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
