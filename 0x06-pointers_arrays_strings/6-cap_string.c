37e80adc057b68c511a31f5e935452581de28261
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-21 12:33:06 +0000

    new commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,26 +10,21 @@
 
 char *cap_string(char *str)
 {
-	int i = 0;
-	int digit = 0;
+	int i = 0, j = 0;
+char s[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 	while (str[i] != '\0')
 	{
-		if (str[i] < 'A' && str[i] > 'z')
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 		{
-			if (str[i] >= '0' && str[i] <= '9')
+			i++;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i - 1] == special[j])
 			{
-				digit = 1;
+				str[i] -= 32;
 			}
-			else if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			{
-				if (!digit)
-				{
-					str[i + 1] = str[i + 1] - 32;
-				}
-				digit = 0;
-			}
-				digit = 0;
 		}
 		i++;
 	}
