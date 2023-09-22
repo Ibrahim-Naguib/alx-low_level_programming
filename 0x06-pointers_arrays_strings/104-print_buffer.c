5c8c7d5fa0f2f1172da0180821b1e8260d4f2169
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-22 21:51:06 +0000

    new commit

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-#include "main.h
+#include "main.h"
 
 void print_buffer(char *b, int size)
 {
-	if (size <= 0) {
+	if (size <= 0)
+	{
 		printf("\n");
 	}
 
-	for (int i = 0; i < size; i += 10) {
+	for (int i = 0; i < size; i += 10)
+	{
 		printf("%08x: ", i);
 
-		for (int j = 0; j < 10; j++) {
+		for (int j = 0; j < 10; j++)
+		{
 			if (i + j < size)
 				printf("%02x", (unsigned char)b[i + j]);
 			else
@@ -19,13 +22,16 @@ void print_buffer(char *b, int size)
 			if (j % 2 != 0)
 				printf(" ");
 		}
-		for (int j = 0; j < 10; j++) {
+		for (int j = 0; j < 10; j++)
+		{
 			if (i + j < size) {
 				if (b[i + j] >= 32 && b[i + j] <= 126)
 					_putchar(b[i + j]);
 				else
 					_putchar('.');
-			} else {
+			}
+			else
+			{
 				break;
 			}
 		}
