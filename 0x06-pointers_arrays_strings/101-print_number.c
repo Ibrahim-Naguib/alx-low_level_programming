b9ede4277671d516a65d928b1ff5fb1a533c689d
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-22 19:09:42 +0000

    new commit

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_number - Print an integer as characters.
+ *
+ * This function takes an integer as input and prints it as a sequence of
+ * characters using _putchar. If the input is negative, a minus sign is printed
+ * first.
+ *
+ * @n: The integer to be printed.
+ */
+
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	int reversed = 0;
+
+	while (n > 0)
+	{
+		int digit = n % 10;
+
+		reversed = reversed * 10 + digit;
+		n /= 10;
+	}
+
+	while (reversed > 0)
+	{
+		int digit = reversed % 10;
+
+		_putchar('0' + digit);
+		reversed /= 10;
+	}
+}
