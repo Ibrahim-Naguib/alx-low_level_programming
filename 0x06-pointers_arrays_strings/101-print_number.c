f454321216e31d50beb4bf5aac2e7e1285498168
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-22 19:36:03 +0000

    new commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,23 +3,19 @@
 /**
  * print_number - Print an integer as characters.
  *
- * This function takes an integer as input and prints it as a sequence of
- * characters using _putchar. If the input is negative, a minus sign is printed
- * first.
- *
  * @n: The integer to be printed.
  */
 
 void print_number(int n)
 {
 	unsigned int n1;
+
 	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		n1 = -n;
 	}
-
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
