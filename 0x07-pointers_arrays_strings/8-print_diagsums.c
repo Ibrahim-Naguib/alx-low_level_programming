c67b7c2a58380bfc4775a3f810b2d9cbce79ed4f
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-25 23:41:06 +0000

    new commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,14 +8,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i, sum1, sum2;
 
+	sum1 = 0;
+	sum2 = 0;
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i];
-		sum2 += a[size - i - 1];
-		a += size;
+		sum1 = sum1 + a[i * size + i];
 	}
-	printf("%d, ", sum1);
-	printf("%d\n", sum2);
+	for (i = size - 1; i >= 0; i--)
+	{
+		sum2 += a[i * size + (size - i - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
