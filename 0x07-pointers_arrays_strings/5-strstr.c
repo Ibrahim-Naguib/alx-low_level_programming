28b0cb1a519fd01cca1a848605da664a670882ed
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-25 23:04:53 +0000

    new commit

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring within a string.
+ * @haystack: The string to search within.
+ * @needle: The substring to search for.
+ *
+ * Return: A pointer to the first occurrence of the substring `needle`
+ * within the string `haystack`, or NULL if `needle` is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+
+			if (haystack[i] == needle[j])
+				return (needle);
+		}
+	}
+	return ('\0');
+}
