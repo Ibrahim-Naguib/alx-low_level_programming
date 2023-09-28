137687faaae42f4bb49bce51b993b9950deabc38
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-09-28 16:46:18 +0000

    new commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Calculate the natural square root of a number recursively
+ * @n: The number for which to calculate the square root
+ *
+ * Return: The natural square root of 'n' or -1 if it doesn't have root.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_recursion(n, 0));
+}
+
+/**
+ * sqrt_recursion - Helper function to recursively calculate the square root
+ * @n: The number for which to calculate the square root
+ * @i: The current guess for the square root
+ *
+ * Return: The natural square root of 'n' or -1 if it doesn't have root.
+ */
+
+int sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqrt_recursion(n, i + 1));
+}
