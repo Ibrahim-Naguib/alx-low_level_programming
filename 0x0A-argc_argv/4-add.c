79ed20556dafc886f5bc5ab15372769a46e91011
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-02 19:21:50 +0000

    new commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,11 +14,6 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc == 1)
-	{
-		printf("%d\n", 0);
-		return (0);
-	}
 	for (i = 1; i < argc; i++)
 	{
 		if (!isdigit(*argv[i]))
@@ -26,8 +21,6 @@ int main(int argc, char *argv[])
 			printf("Error\n");
 			return (1);
 		}
-
-
 		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
