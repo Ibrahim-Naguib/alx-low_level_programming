d157cbf84b81f0cc351b51689cf5b9f79029b8c2
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-03 20:46:21 +0000

    new commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,20 +11,24 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i;
-	int **grid;
+	int i, j, **grid;
 
-	if (width <= 0 || height <= 0)
+	grid = malloc(height * sizeof(*int));
+	if (width <= 0 || height <= 0 || grid == 0)
 		return (0);
 
-	grid = (int **)malloc(height * sizeof(int *));
-	if (grid == NULL)
-		return (0);
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(width * sizeof(int));
-		if (grid[i] == NULL)
+		grid[i] = malloc(width * sizeof(**int));
+		if (grid[i] == 0)
+		{
+			while (i--)
+				free(grid[i]);
+			free(grid);
 			return (0);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
 	}
 	return (grid);
 }
