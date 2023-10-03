c0aba78f75510af5825783784307c34583cc4be5
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-03 19:12:26 +0000

    new commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k = 0, l = 0;
 	char *cat;
 
-	if (s1 == NULL || s2 == NULL)
-		return (" ");
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
