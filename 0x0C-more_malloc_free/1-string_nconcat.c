2a538fe290a690c80b4258f9029c55b505016784
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-07 09:19:19 +0000

    new commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * string_nconcat - Concatenate two strings up to n characters from s2.
+ * @s1: The first string.
+ * @s2: The second string.
+ * @n: The maximum number of characters from s2 to concatenate.
+ *
+ * Return: A pointer to the concatenated string or NULL if malloc fails.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0, j = 0;
+	char *ptr;
+
+	while (s1[i] != '\0')
+		i++;
+	ptr = malloc(i + n + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+
+	j = 0;
+	while (s2[j] != '\0' && j < n)
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+
+	return (ptr);
+}
