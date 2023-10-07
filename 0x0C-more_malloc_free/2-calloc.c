43a6228a3340718e1952d01e213ff010e15a46e7
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-07 13:04:03 +0000

    new commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,6 +13,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
