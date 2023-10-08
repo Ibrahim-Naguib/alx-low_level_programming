6606ee664d584b5ca6cc121985251680d73ef565
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-08 13:04:41 +0000

    new commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,36 +3,6 @@
 #include "main.h"
 #include <unistd.h>
 
-/**
- * main - Multiply two numbers provided as command-line arguments.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings representing the command-line arguments.
- *
- * Return: 0 if successful, 1 if an error occurs.
- */
-
-int main(int argc, char *argv[])
-{
-	unsigned int num1, num2, mul;
-
-	if (argc != 3)
-	{
-		_puts("Error");
-		exit(98);
-	}
-	if (!_isdigit(argv[1]) || !_isdigit(argv[2]))
-	{
-		_puts("Error\n");
-		exit(98);
-	}
-
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	mul = num1 * num2;
-	print_number(mul);
-	_putchar('\n');
-	return (0);
-}
 
 /**
  * _atoi - Converts a string to an integer.
@@ -65,7 +35,8 @@ int _atoi(char *s)
 
 void _puts(char *s)
 {
-	while (*s != '\0') {
+	while (*s != '\0')
+	{
 		_putchar(*s);
 		s++;
 	}
@@ -125,5 +96,36 @@ void print_number(unsigned int n)
 	if ((num / 10) > 0)
 		print_number(num / 10);
 	_putchar((num % 10) + 48);
+}
+
+/**
+ * main - Multiply two numbers provided as command-line arguments.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: 0 if successful, 1 if an error occurs.
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		_puts("Error ");
+		exit(98);
+	}
+	if (!_isdigit(argv[1]) || !_isdigit(argv[2]))
+	{
+		_puts("Error ");
+		exit(98);
+	}
+
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	mul = num1 * num2;
+	print_number(mul);
+	_putchar('\n');
+	return (0);
 }
 
