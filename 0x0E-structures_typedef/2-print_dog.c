47fdd665c0d972dc0ac579a4c49ca4dd1e179e9d
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-10 13:39:22 +0000

    new commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		!d->name ? printf("Name: (nil)\n") : printf("Name: %s\n", d->name);
-		!d->age ? printf("Age: (nil)\n") : printf("Age: %d\n", d->age);
+		!d->age ? printf("Age: (nil)\n") : printf("Age: %.1f\n", d->age);
 		!d->owner ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
 	}
 }
