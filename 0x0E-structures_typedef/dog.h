cefbcfce4a55f1e16193c311ebccdf77b9357edb
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-10 12:31:53 +0000

    new commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct Dog {
+/**
+ * struct Dog - dog details
+ *
+ * @name: pointer to the dog name.
+ * @age: dog's age.
+ * @owner: pointer to the dog owner.
+ */
+
+struct Dog
+{
 	char *name;
 	float age;
 	char *owner;
