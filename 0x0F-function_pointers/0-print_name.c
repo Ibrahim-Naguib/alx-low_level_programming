c98e945f76c602be24e37b335c40eaf5af94de90
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-11 13:03:26 +0000

    new commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,14 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - Print a name using a given function.
+ *
+ * @name: A pointer to a character array (string) representing the name.
+ * @f: A function pointer that takes a char* argument.
+ *
+ * Return: void.
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
