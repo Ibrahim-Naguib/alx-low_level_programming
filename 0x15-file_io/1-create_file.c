d44b565fd0531bb49841d571c5717c42c552baf5
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-30 14:21:48 +0000

    new commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,10 +37,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	if (len)
+	if (text_content != NULL)
 		bytes = write(fd, text_content, len);
 
 	close(fd);
