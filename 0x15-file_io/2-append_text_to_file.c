10a5bb42fa0e10ba0e08fb72c09fdb78c45d1b6e
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2023-10-30 14:42:43 +0000

    new commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int _strlen(char *str)
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes = 0, len = _strlen(text_content);
+	ssize_t bytes = 0, len; 
 
 	if (!filename)
 		return (-1);
@@ -36,7 +36,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-
+	len = _strlen(text_content);
 	if (len)
 		bytes = write(fd, text_content, len);
 
